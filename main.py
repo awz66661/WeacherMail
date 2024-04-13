120a67698f86ae17dc1004f6eac3470d1b39d5d4
awz66661 <2362422778@qq.com>
2024-04-13 09:44:14 +0800

    add more content in mail and modify action

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def main():
     city_name = '上海'
     data = weatherUtils.get_weather(city)
     temperature = weatherUtils.get_temperature(data)
-    content = f"The temperature in {city_name} is {temperature} ℃"
-    sendMailSmtp.send_mail("Weather", content)
+    Weather = weatherUtils.get_info(data, 'weather')
+    reporttime = weatherUtils.get_info(data, 'reporttime')
+    windpower = weatherUtils.get_info(data, 'windpower')
+    humidity = weatherUtils.get_info(data, 'humidity')
+    content = f"城市: {city_name}\n天气: {Weather}\n温度: {temperature}°C\n风力: {windpower}\n湿度: {humidity}\n报告时间: {reporttime}\n"
+    sendMailSmtp.send_mail(f"{reporttime}{city_name}天气", content)
 
 if __name__ == '__main__':
     main()
