0133b569e412a618dd14df77e0add76defd3c851
awz66661 <2362422778@qq.com>
2024-04-14 11:44:54 +0800

    add wyj send

--- main.py
+++ main.py
@@ -14,44 +14,6 @@ def main():
     # Beautify the email content
     content = f"城市: {city_name}\n天气: {Weather}\n温度: {temperature}°C\n风力: {windpower}\n湿度: {humidity}\n报告时间: {reporttime}\n"
     # Beautify the email content as HTML
-    html_content = f"""
-    <html>
-    <head>
-        <style>
-            body {{
-                font-family: Arial, sans-serif;
-                background-color: #f2f2f2;
-            }}
-            h1 {{
-                color: #333;
-                background-color: #fff;
-                padding: 10px;
-                border-radius: 5px;
-            }}
-            .weather-info {{
-                margin-bottom: 20px;
-                background-color: #fff;
-                padding: 10px;
-                border-radius: 5px;
-            }}
-            .weather-info span {{
-                font-weight: bold;
-            }}
-        </style>
-    </head>
-    <body>
-        <h1>{city_name}天气预报</h1>
-        <div class="weather-info">
-            <p><span>城市:</span> {city_name}</p>
-            <p><span>天气:</span> {Weather}</p>
-            <p><span>温度:</span> {temperature}°C</p>
-            <p><span>风力:</span> {windpower}</p>
-            <p><span>湿度:</span> {humidity}</p>
-            <p><span>报告时间:</span> {reporttime}</p>
-        </div>
-    </body>
-    </html>
-    """
 
     html_content1 = f"""
 <html>
@@ -92,7 +54,7 @@ def main():
     </style>
 </head>
 <body>
-        <h1>{city_name}天气预报</h1>
+        <h1>{city_name}天气预报，爱来自猫猫</h1>
         <div class="weather-info">
             <p><span>城市:</span> {city_name}</p>
             <p><span>天气:</span> {Weather}</p>
@@ -109,6 +71,7 @@ def main():
 """
 
     # Send the email with HTML content
+    sendMailSmtp.send_mail(f"{reporttime}{city_name}天气", html_content1, if_html=True, rec="21307140116@m.fudan.edu.cn")
     sendMailSmtp.send_mail(f"{reporttime}{city_name}天气", html_content1, if_html=True)
     #sendMailSmtp.send_mail(f"{reporttime}{city_name}天气", content)
 
