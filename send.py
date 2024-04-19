3a2e0f2568cb313d3d1d50fdf1877ac74de5bf80
awz66661 <2362422778@qq.com>
2024-04-19 11:33:43 +0800

    modify prompt

--- send.py
+++ send.py
@@ -8,10 +8,10 @@ def send(city_name, rec):
     current_date = weatherUtils.get_Current_date()
     Huanlidata = weatherUtils.get_huangli(current_date)
     #{'reason': 'successed', 'result': {'id': '5822', 'yangli': '2024-04-18', 'yinli': '甲辰(龙)年三月初十', 'wuxing': '璧上土', 'chongsha': '冲羊(乙未)煞东', 'baiji': '辛不合酱主人不尝 丑不冠带主不还乡', 'jishen': '月空 天恩 六仪 解神 金匮 鸣犬对', 'yi': '破屋 坏垣 馀事勿取', 'xiongshen': '五虚 月破 大耗 灾煞 天火 四废 厌对 招摇', 'ji': '诸事不宜'}, 'error_code': 0}
-    HuanlidataText = f'{Huanlidata["result"]["yangli"]} {Huanlidata["result"]["yinli"]} {Huanlidata["result"]["wuxing"]} {Huanlidata["result"]["chongsha"]} {Huanlidata["result"]["baiji"]} {Huanlidata["result"]["jishen"]} {Huanlidata["result"]["yi"]} {Huanlidata["result"]["xiongshen"]} {Huanlidata["result"]["ji"]}'
+   #  HuanlidataText = f'{Huanlidata["result"]["yangli"]} {Huanlidata["result"]["yinli"]} {Huanlidata["result"]["wuxing"]} {Huanlidata["result"]["chongsha"]} {Huanlidata["result"]["baiji"]} {Huanlidata["result"]["jishen"]} {Huanlidata["result"]["yi"]} {Huanlidata["result"]["xiongshen"]} {Huanlidata["result"]["ji"]}'
     # get gpt3.5-turbo data
     chat_completion = gptapi.client.chat.completions.create(
-       messages=[{"role": "system", "content": HuanlidataText + "，分析今天的情况，主要是yi和ji。"}],
+       messages=[{"role": "system", "content": str(HuanlidataText) + "，分析今天的情况，主要是宜和忌。"}],
        model="gpt-3.5-turbo",
        max_tokens=200,
        temperature=0.5
