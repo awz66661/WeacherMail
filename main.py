19e372ebbfcf1e3d1601ea6cf88efbf2b6b42b2f
awz66661 <2362422778@qq.com>
2024-04-21 16:25:23 +0800

    test main

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from send import send
-
 if __name__ == '__main__':
-    with open("./users", "r") as f:
-        users = f.read().split('\n')
-        for user in users:
-            print(user+"---------------")
-            city_name, rec = user.split(' ')
-            send(city_name, rec)
+    # get user dict from api
+    import requests
+    import json
+    import os
+    url = 'http://awz66661.icu:8000/users'
+    key = os.environ.get("APIAUTH")
+    # key = "api930080"
+    print(key)
+    params = {
+        "key": key
+    }
+
+    response = requests.get(url, params=params)
+    users = json.loads(response.text)
+    print(users)
+    for rec, city_name in users.items():
+        print(rec, city_name)
+        send(city_name, rec)
--- testAction.py
+++ testAction.py
@@ -2,14 +2,6 @@ from send import send
 import os
 
 if __name__ == '__main__':
-    # with open("./testusers", "r") as f:
-    #     users = f.read().split('\n')
-    #     for user in users:
-    #         print(user+"---------------")
-    #         city_name, rec = user.split(' ')
-    #         send(city_name, rec)
-
-
     # get user dict from api
     import requests
     import json
@@ -22,7 +14,6 @@ if __name__ == '__main__':
     }
 
     response = requests.get(url, params=params)
-   #{"21307130326@m.fudan.edu.cn":"上海市","591141729@qq.com":"北京市"}
     users = json.loads(response.text)
     print(users)
     for rec, city_name in users.items():
